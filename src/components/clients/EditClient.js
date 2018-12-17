9f6b2d9364a63a6a36ef3cc01d3a6bf822f74ae1
noraa <bendicksonaaron@gmail.com>
2018-12-16 19:28:56 -0500

    edit contact functionality added

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -7,11 +7,127 @@ import { firestoreConnect } from "react-redux-firebase";
 import Spinner from "../layout/Spinner";
 
 class EditClient extends Component {
+  constructor(props) {
+    super(props);
+    // Create refs
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.balanceInput = React.createRef();
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore, history } = this.props;
+
+    // Updated Client (what will be submitted to firebase in function below) and since we use ref this will give us the value by using below
+    const updClient = {
+      firstName: this.firstNameInput.current.value,
+      lastName: this.lastNameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value,
+      balance:
+        this.balanceInput.current.value === ""
+          ? 0
+          : this.balanceInput.current.value
+    };
+
+    // Update client in firestore and then redirect user to dashboard
+    firestore
+      .update({ collection: "clients", doc: client.id }, updClient)
+      .then(history.push("/"));
+  };
+
   render() {
     const { client } = this.props;
 
     if (client) {
-      return <div> {client.firstName}</div>;
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+              </Link>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">Edit Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="First Name">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    ref={this.firstNameInput}
+                    defaultValue={client.firstName}
+                  />
+                  {/* key about is that we take firstName from props not state here which is diff than in AddClient form 
+                    we also need to use defaultValue vs value={client.firstName} b/c it's a controlled component 
+                    we also use ref= in order to sync and update to state  and we need a constructor */}
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Last Name">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    ref={this.lastNameInput}
+                    defaultValue={client.lastName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    ref={this.emailInput}
+                    defaultValue={client.email}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="phone"
+                    minLength="10"
+                    required
+                    ref={this.phoneInput}
+                    defaultValue={client.phone}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="balance"
+                    ref={this.balanceInput}
+                    defaultValue={client.balance}
+                  />
+
+                  <input
+                    type="submit"
+                    value="Submit"
+                    className="btn btn-primary btn-block"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
     } else {
       return <Spinner />;
     }
